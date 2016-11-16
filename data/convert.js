98e74368ffa68b83c9d512818d5dd9eb710e7b6a
Serhii <serhii.biliaiev@gmail.com>
2016-11-16 14:21:06 +0200

    convert all routes to arrays

--- data/convert.js
+++ data/convert.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs');
 
 fs.readFile('./danimist.json', function(err, data) {
@@ -5,21 +7,32 @@ fs.readFile('./danimist.json', function(err, data) {
       return console.error(err);
    }
 
-//    console.log(JSON.parse(data));
     let result = JSON.parse(data);
-    // console.log(result[0].routes_way);
+    let finish = [];
 
    for (let i = 0; i < result.length; i++) {
-    //    if (result[i].routes_transport_type === 'trolleybus' && result[i].routes_name === 3) {
         var routes = result[i].routes_way.split(' – ');
         routes.map((item) => `'$(item)`);
-        result[i].routes_way_array = routes;
-        console.log(result[i]);
-        console.log(JSON.stringify(result[i]));
-    //    }
+        result[i].routes_way_forward = routes;
+        if (result[i].routes_direction === 'both') {
+            let routes_back = [];
+            for (let j = routes.length - 1; j >= 0; j--) {
+                routes_back.push(routes[j]);
+            }
+            // routes.reverse();       
+            result[i].routes_way_back = routes_back;
+            finish.push(result[i]);
+        }
+        else if (result[i].routes_direction === 'straight') {
+            let routes_back = result[i+1].routes_way.split(' – ');
+            routes_back.map((item) => `'$(item)`);
+            result[i].routes_way_back = routes_back;
+            finish.push(result[i]);            
+            i++;
+        }
    }
 
-    fs.writeFile(`./converted.json`, JSON.stringify(result, null, 4), 'utf8', function(err) {
+    fs.writeFile(`./danimist_converted_2_arrays.json`, JSON.stringify(finish, null, 4), 'utf8', function(err) {
         if (err) {
             return console.error(err);
         }
